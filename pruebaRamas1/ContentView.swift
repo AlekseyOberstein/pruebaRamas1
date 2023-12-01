d31adec060b0fad28c547c7cc0f9b9223c8b991c
AlekseyOberstein <420035969@pcpuma.acatlan.unam.mx>
2023-12-01 12:28:25 -0600

    Initial Commit

---
+++ pruebaRamas1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pruebaRamas1
+//
+//  Created by Alexis Palomares Olegario on 01/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pruebaRamas1/pruebaRamas1App.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaRamas1App.swift
+//  pruebaRamas1
+//
+//  Created by Alexis Palomares Olegario on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebaRamas1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
